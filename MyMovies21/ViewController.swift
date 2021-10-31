0f9a7116bfb37a96effe857bea1bd3602f9dec47
maddiloven <s0282901@student.rockvalleycollege.edu>
2021-10-31 01:36:23 -0500

    Initial commit

---
+++ MyMovies21/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  MyMovies21
+//
+//  Created by Maddi Loven on 10/30/21.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+        // Get the new view controller using segue.destinationViewController.
+
+        // Pass the selected object to the new view controller.
+
+        if segue.identifier == "first"
+
+        {
+
+            let destination = segue.destination as! PlayerViewController
+
+            //Change to match you video filename in supporting files
+
+            let termsPath:String? = Bundle.main.path(forResource: "trailer1", ofType: "mp4")!
+
+            let url = NSURL(fileURLWithPath: termsPath!)
+
+          
+
+            destination.player = AVPlayer(url: url as URL)
+
+            destination.player!.play()
+
+        }
+
+        if segue.identifier == "second"
+
+        {
+
+            let destination = segue.destination as! PlayerViewController
+
+            //Change to match you video filename in supporting files
+
+            let termsPath:String? = Bundle.main.path(forResource: "trailer2", ofType: "mp4")!
+
+            let url = NSURL(fileURLWithPath: termsPath!)
+
+            destination.player = AVPlayer(url: url as URL)
+
+            destination.player!.play()
+
+        }
+
+        if segue.identifier == "third"
+
+        {
+
+            let destination = segue.destination as! PlayerViewController
+
+            //Change to match you video filename in supporting files
+
+            let termsPath:String? = Bundle.main.path(forResource: "trailer3", ofType: "mp4")!
+
+            let url = NSURL(fileURLWithPath: termsPath!)
+
+            destination.player = AVPlayer(url: url as URL)
+
+            destination.player!.play()
+
+        }
+
+        
+
+    }
+
+    
+    
+}
+
---
+++ PlayerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PlayerViewController.swift
+//  MyMovies21
+//
+//  Created by Maddi Loven on 10/30/21.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class PlayerViewController: AVPlayerViewController {
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscapeLeft
+    }
+}
